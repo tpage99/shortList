ef512a89c4ab0acca7ff80cd1bae9bfe43c4e2ac
tpage99 <taylorvtx1300@gmail.com>
2018-10-27 08:44:03 -0500

    updated mongoose. made some reformats

--- server/src/models.js
+++ server/src/models.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose")
-const moment = require('moment')
+const mongoose = require('mongoose');
+const moment = require('moment');
 const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
 
